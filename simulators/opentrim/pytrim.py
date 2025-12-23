a53cfe7b685a080a05225c93b2aaf32ad0908f9a
Proshutinskiy Sergey <hrvergewrbhfek@gmail.com>
2025-12-23 17:16:13 +0100

    Added time adaptive simulation
    - Can be used to save results every N seconds

--- simulators/opentrim/pytrim.py
+++ simulators/opentrim/pytrim.py
@@ -52,8 +52,21 @@ sim_params = SimParams( nspec = 2,
                         limits = (0.0, 4000.0))
 statistics.setup(nspec=sim_params.nspec, nbin=sim_params.nbin, limits=sim_params.limits)
 
-@jit(fastmath=True, cache=True, parallel=True, nogil=True)
+@jit(fastmath=True, cache=False, parallel=True, nogil=True)
 def simulate(nion, sim_params_tup, follow_recoils=False):
+    """Perform simulation on given number of projectiles
+    
+    Parameters:
+        nion: (int) Total number of projectiles to simulate
+        sim_params_tup: (tuple) Simulation parameters (provided by `SimParams.to_tuple()`)
+        follow_recoils: (bool) If the simulation should be performed for recoils aswell
+        
+    Returns:
+        tuple[int, np.ndarray, np.ndarray]:
+            Total number of simulated projectiles,
+            Result buffer for `Histogram_1d` class,
+            Result buffer for `Moment_1d` class
+    """
     # Initial conditions of the projectile
     proj_init = Projectile(
         50000.0,                         # energy (eV)
@@ -84,26 +97,101 @@ def simulate(nion, sim_params_tup, follow_recoils=False):
                 mom.score(proj.ispec, proj.pos[2])
     return proj_count, hist.results, mom.results
 
-if __name__ == "__main__":
-    from tqdm import tqdm
+def simulate_adaptive(avg_chunk_time, nion, *args, **kwargs):
+    """Adaptive, chunked simulation with each chunk taking around avg_sim_time seconds
     
+    Parameters:
+        avg_sim_time: (int) Desired simulation time per in seconds
+        nion: (int) Total number of projectiles to simulate
+        *args, **kwargs: As in `simulate()`
+        
+    Returns:
+        tuple[int, np.ndarray, np.ndarray]:
+            Total number of simulated projectiles,
+            Result buffer for `Histogram_1d` class,
+            Result buffer for `Moment_1d` class
+    """
+    min_chunk_size = 100
+    chunk_size = min_chunk_size
+    processed_count = 0
+    
+    total_proj_count = 0
+    total_hist_buf = None
+    total_mom_buf = None
+
+    while processed_count < nion:
+        current_batch = min(chunk_size, nion - processed_count)
+        
+        start_time = time.time()
+        proj_count, hist_buf, mom_buf = simulate(current_batch, *args, **kwargs)
+        # NOTE: Saving can be performed here
+        
+        total_proj_count += proj_count
+        if total_hist_buf is None:
+            total_hist_buf = hist_buf.copy()
+            total_mom_buf = mom_buf.copy()
+        else:
+            total_hist_buf += hist_buf
+            total_mom_buf += mom_buf  # pyright: ignore[reportOperatorIssue]
+        duration = time.time() - start_time
+        
+        processed_count += current_batch
+        # Calculate optimal chunk size
+        new_chunk = int((current_batch / duration) * avg_chunk_time)
+        chunk_size = max(min_chunk_size, new_chunk)
+    
+    return total_proj_count, total_hist_buf, total_mom_buf
+
+def simulate_chunked(chunk_size, nion, *args, **kwargs):
+    """Chunked simulation for nion projectiles
+    
+    Parameters:
+        chunk_size: (int) Size to split total count into
+        nion: (int) Total number of projectiles to simulate
+        *args, **kwargs: As in `simulate()`
+        
+    Returns:
+        tuple[int, np.ndarray, np.ndarray]:
+            Total number of simulated projectiles,
+            Result buffer for `Histogram_1d` class,
+            Result buffer for `Moment_1d` class
+    """
+    assert nion % chunk_size == 0, "Total projectile count must be a multiple of chunk size"
+    
+    total_proj_count = 0
+    total_hist_buf = None
+    total_mom_buf = None
+    for _ in range(0, nion, chunk_size):
+        proj_count, hist_buf, mom_buf = simulate(chunk_size, *args, **kwargs)
+        # NOTE: Saving can be performed here
+        
+        if total_hist_buf is None:
+            total_hist_buf = hist_buf.copy()
+            total_mom_buf = mom_buf.copy()
+        else:
+            total_hist_buf += hist_buf
+            total_mom_buf += mom_buf  # pyright: ignore[reportOperatorIssue]
+        total_proj_count += proj_count
+    return total_proj_count, total_hist_buf, total_mom_buf
+
+if __name__ == "__main__":
     times = []
     proj_counts = []
     counts = [1000, 10000]
-    save_every = 100
+    # chunk_size = 100
+    avg_chunk_time = 0.1    # seconds
     simulate(10, sim_params.to_tuple(), follow_recoils=True)    # pre-compile
     for i, c in enumerate(counts):
         # empty stats for each nion count
         statistics.setup(nspec=sim_params.nspec, nbin=sim_params.nbin, limits=sim_params.limits)
-        times.append(0)
-        proj_counts.append(0)
-        for proj_done in tqdm(range(0, c, save_every), leave=False):
-            start_time = time.time()
-            proj_count, hist_buf, mom_buf = simulate(save_every, sim_params.to_tuple(), follow_recoils=True)
-            statistics.hist.results += hist_buf
-            statistics.mom.results += mom_buf
-            times[i] += time.time() - start_time
-            proj_counts[i] += proj_count
+        
+        start_time = time.time()
+        proj_count, hist_buf, mom_buf = simulate_adaptive(avg_chunk_time, c, sim_params.to_tuple(), follow_recoils=True)
+        # proj_count, hist_buf, mom_buf = simulate_chunked(chunk_size, c, sim_params.to_tuple(), follow_recoils=True)
+        statistics.hist.results = hist_buf
+        statistics.mom.results = mom_buf
+        times.append(time.time() - start_time)
+        proj_counts.append(proj_count)
     print(times, proj_counts)
     
     # Output the results
